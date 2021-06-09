7d91eee65b6c4c979e491a29bd180cde6a546b29
danhhuynh25029 <danhhuynh250@gmail.com>
2021-06-10 00:14:06 +0700

    first commit

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import cv2
 import numpy as np
-def filter(frame,x,y,w,h):
-    hair = cv2.imread("clown.png") 
-    hair = cv2.resize(hair,(int(w*1),int(h*1)))
-    for i in range(int(w*1)):
-        for j in range(int(h*1)):
-            # color RGBA 
+def filterHeart(frame,x,y,w,h):
+    hair = cv2.imread("heart.png")
+    hair = cv2.resize(hair,(w,100))
+    for i in range(100):
+        for j in range(w):
+            # color RGBA
+            #print(i,j)
             if hair[i][j][2] != 0: # a != 0
                 frame[i][x+j] = hair[i][j]            
                 
@@ -13,13 +14,13 @@ def detectFace(faceCascade,eyeCascade,frame):
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(gray,1.3,5)
     for x,y,w,h in faces:
-        filter(frame,x,y,w,h)
-       # cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-       # r_gray = gray[y:y+h,x:x+w]
-       # r_color = frame[y:y+h,x:x+w]
-       # eyes = eyeCascade.detectMultiScale(r_gray)
-       # for ex,ey,ew,eh in eyes:
-        #    cv2.rectangle(r_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        filterHeart(frame,x,y,w,h)
+        #cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+        r_gray = gray[y:y+h,x:x+w]
+        r_color = frame[y:y+h,x:x+w]
+        eyes = eyeCascade.detectMultiScale(r_gray)
+        for ex,ey,ew,eh in eyes:
+            cv2.rectangle(r_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 def openVideo(cap):
     if cap.isOpened() == False:
         print("error video")
