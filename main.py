bb97c8b2ae0cba69237b959afba1e6f2975fc806
danhhuynh25029 <danhhuynh250@gmail.com>
2021-06-10 13:15:06 +0700

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import cv2
 import numpy as np
+def filterMask(fram,x,y,w,h):
+    pass
+def filterGlass(frame,ex,ey,ew,eh,x,y,w,h):
+    glass = cv2.imread("sunglasses.png")
+    #glass = cv2.cvtColor(glass,cv2.COLOR_BGR2BGRA)
+    glass = cv2.resize(glass,(w,50))
+    for i in range(50):
+        for j in range(w):
+            if glass[i][j][2] != 0:
+                frame[i+ey+y][x+j] = glass[i][j]                
+    #cv2.circle(frame,(w//2+x,h//2+y),10,(0,255,0),2)
+    
 def filterHeart(frame,x,y,w,h):
     hair = cv2.imread("heart.png")
     hair = cv2.resize(hair,(w,100))
@@ -8,19 +20,22 @@ def filterHeart(frame,x,y,w,h):
             # color RGBA
             #print(i,j)
             if hair[i][j][2] != 0: # a != 0
-                frame[i][x+j] = hair[i][j]            
+                frame[y-50+i][x+j] = hair[i][j]            
                 
 def detectFace(faceCascade,eyeCascade,frame):
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(gray,1.3,5)
     for x,y,w,h in faces:
-        filterHeart(frame,x,y,w,h)
-        #cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+        # filter Heart
+        #filterHeart(frame,x,y,w,h)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
         r_gray = gray[y:y+h,x:x+w]
         r_color = frame[y:y+h,x:x+w]
         eyes = eyeCascade.detectMultiScale(r_gray)
         for ex,ey,ew,eh in eyes:
-            cv2.rectangle(r_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            filterGlass(frame,ex,ey,ew,eh,x,y,w,h)
+            #cv2.circle(frame,(x+ex+ew//2,y+ey+eh//2),20,(0,255,0),2)
+            break
 def openVideo(cap):
     if cap.isOpened() == False:
         print("error video")
